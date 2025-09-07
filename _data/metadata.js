8f44d7cc635cc793f2e7c5929190aa4be7bdae8a
bmrtech-oss <bmrtech@myyahoo.com>
2025-09-07 19:57:18 +0530

    Update _data/metadata.js (via Pages CMS)

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Bunty Rathod's Tech Blog",
+	url: "https://bmrtech-blog.pages.dev/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my experiences as a computer engineer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Bunty Rathod",
+		email: "bunty.m.rathod@gmail.com",
+		url: "https://bmrtech-blog.pages.dev/about/"
 	}
 }
