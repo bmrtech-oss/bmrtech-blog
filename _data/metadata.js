bfcb369904241f8be3d746a2db64f01a84b7f2e3
Pramod Rathod <bmrtech@myyahoo.com>
2025-12-25 20:36:54 +0530

    Update _data/metadata.js (via Pages CMS)

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Antinode Labs",
-	url: "https://bmrtech-blog.pages.dev/",
+	url: "https://antinodelabs.com/",
 	language: "en",
 	description: "I am writing about my experiences as a computer engineer and lifelong learner.",
 	author: {
-		name: "Bunty Rathod",
-		email: "bunty.m.rathod@gmail.com",
-		url: "https://bmrtech-blog.pages.dev/about/"
+		name: "Antinode Labs",
+		email: "info@antinodelabs.con",
+		url: "https://antinodelabs.com/about"
 	}
 }
